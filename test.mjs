0cd9fb681887672834026e58ac61264224e922fa
shweta <135203546+shweta-fable@users.noreply.github.com>
2023-12-01 12:46:41 +0530

    Update test.mjs

--- test.mjs
+++ test.mjs
@@ -1,1 +1,19 @@
-# heading
+ import React from 'https://cdn.skypack.dev/react';
+  import ReactDOM from 'https://cdn.skypack.dev/react-dom';
+
+  function App() {
+    const [count, setCount] = React.useState(0);
+
+    const clicked = () => {
+      setCount(count + 1)
+    }
+
+    return (
+      <div className='container'>
+        <div>Testing</div>
+        <button onClick={clicked}>Clicked me times {count}</button>
+      </div>
+    );
+  }
+
+  ReactDOM.render(<App />, document.body);
