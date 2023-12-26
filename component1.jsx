d0714defa08e3e9144c79c12433d07e7d91d1b67
Shweta <shweta@sharefable.com>
2023-12-26 11:28:36 +0530

    use component

--- component1.jsx
+++ component1.jsx
@@ -1,17 +1,17 @@
-import React from "https://esm.sh/react"
-import confetti from "https://esm.sh/canvas-confetti@1.6.0"
-import ReactDom from "https://esm.sh/react-dom@18.2.0"
+import React from "react"
+// import confetti from "https://esm.sh/canvas-confetti@1.6.0"
+// import ReactDom from "https://esm.sh/react-dom@18.2.0"
 // import './code.css';
 
 const Component1 = () => {
   function onMouseMove(e) {
-    confetti({
-      particleCount: 5,
-      origin: {
-        x: e.pageX / window.innerWidth,
-        y: (e.pageY + 20) / window.innerHeight,
-      }
-    })
+    // confetti({
+    //   particleCount: 5,
+    //   origin: {
+    //     x: e.pageX / window.innerWidth,
+    //     y: (e.pageY + 20) / window.innerHeight,
+    //   }
+    // })
   }
 
   return (
