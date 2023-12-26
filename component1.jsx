13be454190f9bed0f78a986ffe1c2bc3954c710b
shweta <135203546+shweta-fable@users.noreply.github.com>
2023-12-26 11:17:30 +0530

    Create component1.jsx

---
+++ component1.jsx
@@ -0,0 +1,25 @@
+import React from "https://esm.sh/react"
+import confetti from "https://esm.sh/canvas-confetti@1.6.0"
+import ReactDom from "https://esm.sh/react-dom@18.2.0"
+// import './code.css';
+
+const Component1 = () => {
+  function onMouseMove(e) {
+    confetti({
+      particleCount: 5,
+      origin: {
+        x: e.pageX / window.innerWidth,
+        y: (e.pageY + 20) / window.innerHeight,
+      }
+    })
+  }
+
+  return (
+    <div onMouseMove={onMouseMove}>
+      <h1>Hello React! ⚛️</h1>
+      <p>Building user interfaces.</p>
+    </div>
+  )
+}
+
+export default Component1
